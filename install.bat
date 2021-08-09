1c41d7a56cd4e2ba9f57a74edbd96169d4200208
yuailong <yuailong@tal.com>
2021-08-09 11:34:19 +0800

    Adding installation script for this tool

---
+++ install.bat
@@ -0,0 +1,15 @@
+@echo off
+
+: install qt-conan-tools-qmake
+
+net file 1>NUL 2>NUL & if ERRORLEVEL 1 (echo You must right-click and select "RUN AS ADMINISTRATOR"  to run this batch. Exiting... & echo. & pause & exit)
+
+setx -m CONAN_QT_QMAKE_HOME %~dp0
+setx -m applyConanPlugin %~dp0conan.pri
+
+echo.
+echo Conan Qt Plugin has been installed successfully. You can call 'include($$(applyConanPlugin))' or 'include($$(CONAN_QT_QMAKE_TOOL)/conan.pri)' in your profile to enable conan plugin, more detail check https://github.com/Fishoo0/qt-conan-tools-qmake.
+echo.
+
+pause
+exit
