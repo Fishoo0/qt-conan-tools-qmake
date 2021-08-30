d52c48bdb068517e9803d16c5b5716e8ae680a2b
yuailong <yuailong@tal.com>
2021-08-30 18:07:52 +0800

    update plugin

--- install.bat
+++ install.bat
@@ -12,7 +12,7 @@ setx -m QT_QMAKE_EXTENSION_HOME %INSTALL_DIR%
 setx -m CONAN_QT_QMAKE_HOME %INSTALL_DIR%\conan
 setx -m applyConanPlugin %INSTALL_DIR%\conan\conan.pri
 setx -m applyCommonConfig %INSTALL_DIR%\config\commonConfig.pri
-setx -m setGitRevision %INSTALL_DIR%\utils\gitRevision.pri
+setx -m setGitRevision %INSTALL_DIR%\utils\setGitRevision.pri
 
 echo.
 echo Conan Qt Plugin has been installed successfully, more detail check https://github.com/Fishoo0/qt-conan-tools-qmake.
--- utils/gitRevision.pri
+++
@@ -1,8 +0,0 @@
-REVISION = $$(CI_COMMIT_SHA)
-isEmpty(REVISION) {
-    REVISION = $$system("git rev-parse HEAD")
-}
-isEmpty(REVISION) {
-    REVISION = Unknown
-}
-DEFINES += GIT_REVISION=\\\"$$REVISION\\\"
