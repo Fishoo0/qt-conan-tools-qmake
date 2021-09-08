2bea7192c5f9f4ecd723801e7939972fa195fa4e
yuailong <yuailong@tal.com>
2021-09-08 18:25:47 +0800

    Adapt for Mac

--- conan/conanInstall.pri
+++ conan/conanInstall.pri
@@ -1,24 +1,21 @@
 # install conan libs
 # if you want "conan install --update", specify CONAN_INSTALL_UPDATE = true in your calling pro
 !build_pass {
-    win32 {
-        CONFIG(release, debug|release) {
-            CONAN_BUILD_TYPE = Release
-        } else {
-            CONAN_BUILD_TYPE = Debug
-        }
-        contains(QT_ARCH, i386) {
-           CONAN_ARCH = x86
-        } else {
-           CONAN_ARCH = x86_64
-        }
-        conan_install_cmd_params = $$CONAN_INSTALL_PARAMS
-
-        CONAN_QT_VERSION = $$[QT_VERSION]
-        conan_install_cmd =  "conan install $$_PRO_FILE_PWD_ -s build_type=$$CONAN_BUILD_TYPE -s arch=$$CONAN_ARCH -s arch_build=$$CONAN_ARCH -o *:qt_version=$$CONAN_QT_VERSION -if $$shadowed($$_PRO_FILE_PWD_) $$conan_install_cmd_params"
-        message($$conan_install_cmd)
-        system($$conan_install_cmd)
+    CONFIG(release, debug|release) {
+        CONAN_BUILD_TYPE = Release
+    } else {
+        CONAN_BUILD_TYPE = Debug
     }
+    contains(QT_ARCH, i386) {
+       CONAN_ARCH = x86
+    } else {
+       CONAN_ARCH = x86_64
+    }
+    conan_install_cmd_params = $$CONAN_INSTALL_PARAMS
+    CONAN_QT_VERSION = $$[QT_VERSION]
+    conan_install_cmd =  "conan install $$_PRO_FILE_PWD_ -s build_type=$$CONAN_BUILD_TYPE -s arch=$$CONAN_ARCH -s arch_build=$$CONAN_ARCH -o *:qt_version=$$CONAN_QT_VERSION -if $$shadowed($$_PRO_FILE_PWD_) $$conan_install_cmd_params"
+    message($$conan_install_cmd)
+    system($$conan_install_cmd)
 }
 
 # optional: Convenient configure for QtCreator displaying conan file list
--- install.sh
+++ install.sh
@@ -5,16 +5,22 @@ rm -rf $INSTALL_DIR
 mkdir -p $INSTALL_DIR
 cp -r * $INSTALL_DIR
 
-echo '#START_OF_QT_QMAKE_EXTENSION' >> ~/.profile
-echo export QT_QMAKE_EXTENSION_HOME=$INSTALL_DIR >> ~/.profile
-echo export applyConanPlugin=$INSTALL_DIR/conan/conan.pri >> ~/.profile
-echo export applyCommonConfig=$INSTALL_DIR/config/commonConfig.pri >> ~/.profile
-echo export applyDestDirConfig=$INSTALL_DIR/config/destDirConfig.pri >> ~/.profile
-echo export setGitRevision=$INSTALL_DIR/utils/setGitRev.pri >> ~/.profile
-echo '#END_OF_QT_QMAKE_EXTENSION' >> ~/.profile
+echo '#START_OF_QT_QMAKE_EXTENSION' > ~/.conan_profile
+echo export QT_QMAKE_EXTENSION_HOME=$INSTALL_DIR >> ~/.conan_profile
+echo export applyConanPlugin=$INSTALL_DIR/conan/conan.pri >> ~/.conan_profile
+echo export applyCommonConfig=$INSTALL_DIR/config/commonConfig.pri >> ~/.conan_profile
+echo export applyDestDirConfig=$INSTALL_DIR/config/destDirConfig.pri >> ~/.conan_profile
+echo export setGitRevision=$INSTALL_DIR/utils/setGitRev.pri >> ~/.conan_profile
+echo '#END_OF_QT_QMAKE_EXTENSION' >> ~/.conan_profile
+
+if ! grep -q '.conan_profile' ~/.profile; then
+  echo  source ~/.conan_profile >> ~/.profile
+fi
+source ~/.profile
 
 echo Conan Qt Plugin has been installed successfully, more detail check https://github.com/Fishoo0/qt-conan-tools-qmake.
 echo call 'include($$(applyConanPlugin))' in your pro file to enable conan plugin
 echo call 'include($$(applyCommonConfig))' in your pro file to enable common config
 echo call 'include($$(applyDestDirConfig))' in your pro file to enable dest dir config
 echo call 'include($$(setGitRevision))' in your pro file to set GIT_REVISION of your project
+
